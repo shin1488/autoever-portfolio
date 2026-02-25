c3fe40567cc840fa9ca272ac57385c1df9b0537a
shin1488 <syc1488@naver.com>
2026-02-26 06:24:55 +0900

    추가 : Header 기본 뼈대 및 스타일 작성

--- 신영찬/shin-portfolio/src/App.tsx
+++ 신영찬/shin-portfolio/src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
-
+      <Header>
+        
+      </Header>
     </div>
   )
 }
---
+++ 신영찬/shin-portfolio/src/components/Header.tsx
@@ -0,0 +1,21 @@
+import styles from './Header.module.css';
+import logo from '../assets/logo.png';
+
+const Header = () => {
+    return (
+        <header className={styles.header}>
+            <div className={styles.title_section}>
+                <img className={styles.logo} src={logo}></img>
+                <p className={styles.title}>신영찬의 포트폴리오</p>
+            </div>
+            <div className={styles.nav_section}>
+                <p>Home</p>
+                <p>Projects</p>
+                <p>Blog</p>
+                <p>GuestBook</p>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
