2e1163d6f11d2b91f7b7e827d66963a37f364f2b
shin1488 <syc1488@naver.com>
2026-02-26 15:08:39 +0900

    변경 : bottom nav bar에서 LinkedIn 클릭 시 새 창으로 열도록 변경

--- 신영찬/shin-portfolio/src/components/BottomNavBar/BottomNavBar.tsx
+++ 신영찬/shin-portfolio/src/components/BottomNavBar/BottomNavBar.tsx
@@ -4,8 +4,8 @@ const BottomNavBar = () => {
     return (
         <div className={styles.nav_container}>
             <a href="mailto:shin1488dev@gmail.com">Email</a>
-            <a href="https://github.com/shin1488" target="_blank">Github</a>
-            <a href="https://www.linkedin.com/in/영찬-신-9aa85b3b3">LinkedIn</a>
+            <a href="https://github.com/shin1488" target='_blank'>Github</a>
+            <a href="https://www.linkedin.com/in/영찬-신-9aa85b3b3" target='_blank'>LinkedIn</a>
         </div>
     );
 }
