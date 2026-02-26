f9538c8bea031e3b5cabf4c2c017e9847b004de9
shin1488 <syc1488@naver.com>
2026-02-26 12:30:45 +0900

    변경 : props 타입을 any로 지정

--- 신영찬/shin-portfolio/src/containers/SkillContainer/SkillContainer.tsx
+++ 신영찬/shin-portfolio/src/containers/SkillContainer/SkillContainer.tsx
@@ -3,7 +3,7 @@ import styles from './SkillContainer.module.css';
 
 const SkillContainer = ({ items }: any) => (
     <div className={styles.container}>
-        {items.map(item => (
+        {items.map((item: any) => (
             <SkillIcon img={item.logo} name={item.name}/>
         ))}
     </div>
