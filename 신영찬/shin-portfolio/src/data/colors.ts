4904be59e2f95d4214bce1f3abb81300da79ecbd
shin1488 <syc1488@naver.com>
2026-02-27 00:01:37 +0900

    추가: color 타입 지정 및 css 색상 변수화

---
+++ 신영찬/shin-portfolio/src/data/colors.ts
@@ -0,0 +1,10 @@
+import type { Colors } from "../types/colors";
+
+export const COLORS = {
+  primary: '#0046FF',
+  secondary: '#99ADFF',
+  hover: '#336BFF',
+  click: '#0038CC',
+  card_background: '#E6EDFF',
+  blue_black: '#374151',
+} as const satisfies Colors;
---
+++ 신영찬/shin-portfolio/src/types/colors.ts
@@ -0,0 +1,10 @@
+export type ColorKey = 'primary' | 'secondary' | 'hover' | 'click' | 'card_background' | 'blue_black';
+
+export interface Colors {
+  primary: string;
+  secondary: string;
+  hover: string;
+  click: string;
+  card_background: string;
+  blue_black: string;
+}
