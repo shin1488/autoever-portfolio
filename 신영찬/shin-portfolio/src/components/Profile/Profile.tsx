6c651e2cf36795d8da81b8da9a26b9b13a60216f
shin1488 <syc1488@naver.com>
2026-02-26 12:39:10 +0900

    변경 : info 레이아웃 수정

--- 신영찬/shin-portfolio/src/components/Profile/Profile.tsx
+++ 신영찬/shin-portfolio/src/components/Profile/Profile.tsx
@@ -8,13 +8,11 @@ const Profile = () => {
             <div className={styles.introduce_section}>
                 <p className={styles.name}>신영찬</p>
                 <p className={styles.job}>Android Developer</p>
-                <p className={styles.info}>
-                    Birth: 1998-06-25
-                    <br />
-                    Contact: 010-3144-1488
-                    <br />
-                    Email: syc1488@naver.com
-                </p>
+                <div className={styles.info}>
+                    <p>Birth: 1998-06-25</p>
+                    <p>Contact: 010-3144-1488</p>
+                    <p>Email: syc1488@naver.com</p>
+                </div>
             </div>
         </div>
     );
