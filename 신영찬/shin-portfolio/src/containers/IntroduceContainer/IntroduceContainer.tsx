5f417cb710b80f744a6a9440357c98d0d696da75
shin1488 <syc1488@naver.com>
2026-02-26 10:52:41 +0900

    추가 : Introduce 컴포넌트 작성 및 IntroduceContainer 마무리

--- 신영찬/shin-portfolio/src/containers/IntroduceContainer/IntroduceContainer.tsx
+++ 신영찬/shin-portfolio/src/containers/IntroduceContainer/IntroduceContainer.tsx
@@ -1,3 +1,4 @@
+import Introduce from '../../components/Introduce/Introduce';
 import Profile from '../../components/Profile/Profile';
 import styles from './IntroduceContainer.module.css';
 
@@ -5,6 +6,7 @@ const IntroduceContainer = () => {
     return (
         <div className={styles.container}>
             <Profile />
+            <Introduce />
         </div>
     );
 }
